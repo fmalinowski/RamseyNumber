cc447d3d45242e988f250745ed74ece6a0682a91
ethanpreble <ethanpreble@gmail.com>
2015-05-15 16:59:13 -0700

    added a very simple standalone client

---
+++ apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/standalone/StandaloneClient.java
@@ -0,0 +1,25 @@
+package edu.ucsb.cs290cloud.standalone;
+
+import edu.ucsb.cs290cloud.commons.GraphFactory;
+import edu.ucsb.cs290cloud.graphdao.GraphDao;
+import edu.ucsb.cs290cloud.graphdao.GraphDaoParse;
+import edu.ucsb.cs290cloud.strategies.Strategy;
+import edu.ucsb.cs290cloud.strategies.Strategy1Random;
+
+/**
+ * Created by ethan_000 on 5/15/2015.
+ */
+public class StandaloneClient {
+    public static void main(String[] args)
+    {
+        Strategy s = new Strategy1Random();
+        s.setInitialGraph(GraphFactory.generateRandomGraph(Integer.parseInt(args[0])));
+        s.run();
+        GraphDao dao = new GraphDaoParse();
+        if(s.getStrategyStatus()== Strategy.Status.COUNTER_EXAMPLE)
+        {
+            dao.storeGraph(s.getGraph());
+        }
+
+    }
+}
