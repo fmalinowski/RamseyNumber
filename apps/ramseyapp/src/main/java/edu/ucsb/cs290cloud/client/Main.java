55e56bfc118cec21f36fcccc92f3217e34c20776
Francois Malinowski <francois@francois-malinowski.fr>
2015-05-15 01:35:02 -0700

    fm - Add command line option for client to provide IP of master node

--- apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/client/Main.java
+++ apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/client/Main.java
@@ -1,5 +1,11 @@
 package edu.ucsb.cs290cloud.client;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import edu.ucsb.cs290cloud.strategies.Strategy1;
 
 public class Main {
@@ -9,8 +15,32 @@ public class Main {
 	
 	// start a client here
 	public static void main(String[] args) {
+		Options options;
+		CommandLineParser parser;
+		CommandLine cmd;
+		String ipMasterNode;
+		
+		// Create the command line parser
+		parser = new DefaultParser();
+				
+		// Create the options
+		options = new Options();
+		options.addOption("i", "ip", true, "IP of master node. --ip=128.0.0.1 Default is localhost");
+				
+		cmd = null;
+		// Parse the command line arguments
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		ipMasterNode = cmd.hasOption("i") ? cmd.getOptionValue("i") : IP_DEFAULT;
+		
+		System.out.println("IP server: " + ipMasterNode);
+		
 		Client client;		
-		client = new Client(PORT_DEFAULT, IP_DEFAULT, Strategy1.class);
+		client = new Client(PORT_DEFAULT, ipMasterNode, Strategy1.class);
 		client.run();
 	}
 }
