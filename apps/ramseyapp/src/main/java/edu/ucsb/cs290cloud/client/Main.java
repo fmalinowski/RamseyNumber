7a2945c79e7da83392dc02bda2b2cc97023ef003
Francois Malinowski <francois@francois-malinowski.fr>
2015-05-17 14:42:46 -0700

    fm - add options in Client CLI to set how the matrix should be split between the clients and also which position in matrix the client should focus on. Set best clique count to be the one received by master node in strategy 1 and strategy1distributed

--- apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/client/Main.java
+++ apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/client/Main.java
@@ -7,6 +7,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import edu.ucsb.cs290cloud.strategies.Strategy1;
+import edu.ucsb.cs290cloud.strategies.Strategy1Distributed;
 import edu.ucsb.cs290cloud.strategies.Strategy1Random;
 
 public class Main {
@@ -24,6 +25,7 @@ public class Main {
 		String ipMasterNode;
 		String clientStrategy;
 		Class strategyClass;
+		int matrixSplitsNb, clientPosInSplittedMatrix;
 		
 		// Create the command line parser
 		parser = new DefaultParser();
@@ -32,6 +34,8 @@ public class Main {
 		options = new Options();
 		options.addOption("i", "ip", true, "IP of master node. --ip=128.0.0.1 Default is localhost");
 		options.addOption("s", "strategy", true, "Strategy to be used. --strategy=strategy1");
+		options.addOption("n", "nsplit", true, "Matrix split between N clients. --nsplit=N");
+		options.addOption("p", "pos", true, "Position of client in splitted matrix. Position must be between 1 and N. --pos=2");
 				
 		cmd = null;
 		// Parse the command line arguments
@@ -44,16 +48,25 @@ public class Main {
 		ipMasterNode = cmd.hasOption("i") ? cmd.getOptionValue("i") : IP_DEFAULT;
 		clientStrategy = cmd.hasOption("s") ? cmd.getOptionValue("s") : DEFAULT_STRATEGY;
 		
+		matrixSplitsNb = cmd.hasOption("n") ?  
+				Integer.parseInt(cmd.getOptionValue("n")) : 1;
+				
+		clientPosInSplittedMatrix = cmd.hasOption("p") ? 
+				Integer.parseInt(cmd.getOptionValue("p")) : 1;
+		
 		strategyClass = getStrategyClass(clientStrategy);
 		
 		Client client;		
-		client = new Client(PORT_DEFAULT, ipMasterNode, strategyClass);
+		client = new Client(PORT_DEFAULT, ipMasterNode, strategyClass, matrixSplitsNb, clientPosInSplittedMatrix);
 		client.run();
 	}
 	
 	public static Class getStrategyClass(String strategyString) {
 		if (strategyString.equalsIgnoreCase("strategy1random")) {
 			return Strategy1Random.class;
+		}
+		else if (strategyString.equalsIgnoreCase("strategy1distributed")) {
+			return Strategy1Distributed.class;
 		}
 		else {
 			return Strategy1.class;
