9e0f03d5e60de691ff815185cd366365c423e3c8
fmalinowski <francois@francois-malinowski.fr>
2015-05-22 18:14:39 -0700

    Revert "fm - add command line option to generate an initial graph instead of …"

--- apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/server/Main.java
+++ apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/server/Main.java
@@ -32,8 +32,7 @@ public class Main {
 		
 		// Create the options
 		options = new Options();
-		options.addOption("l", "load", true, "How to load the counter examples. --load=remote being graphs stored remotely. --load=disk being graphs stored on disk --load=generate");
-		options.addOption("s", "size", true, "Size of the initial graph being generated in case the laod option is generate.");
+		options.addOption("l", "load", true, "How to load the counter examples. --load=remote being graphs stored remotely. --load=disk being graphs stored on disk");
 		
 		cmd = null;
 		// Parse the command line arguments
@@ -45,9 +44,6 @@ public class Main {
 		
 		if (cmd.hasOption("l") && cmd.getOptionValue("l").equals("disk")) {
 			graphsExplorer.loadGraphsFromDisk();
-		}
-		else if(cmd.hasOption("l") && cmd.getOptionValue("l").equals("generate") && cmd.hasOption("s")) {
-			graphsExplorer.generateInitialGraph(Integer.parseInt(cmd.getOptionValue("s")));
 		}
 		else {
 			graphsExplorer.loadGraphsFromRemoteService();
