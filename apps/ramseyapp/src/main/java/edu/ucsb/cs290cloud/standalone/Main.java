42732b42252ef08c3afd7978fd3ef2f6f3e8186e
ethanpreble <ethanpreble@gmail.com>
2015-05-22 18:06:32 -0700

    adding a running standalone main

--- apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/standalone/Main.java
+++ apps/ramseyapp/src/main/java/edu/ucsb/cs290cloud/standalone/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		StandaloneClient.main(args);
+
+
+
 		// new CounterExamplesFinder().startStrategy1_random(null);
 //		new CounterExamplesFinder().startStrategy2(null);
 	}
